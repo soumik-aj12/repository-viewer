b27446feba6265a7dd7a2a7aee278ce40e25ad01
Soumik Sil <soumiksilco@gmail.com>
2024-01-03 19:24:13 +0530

    Fixed a minor bug in Pagination component

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -11,7 +11,7 @@ export const Pagination = (props) => {
         <ul className="list-none flex space-x-2">
          {pageNums.map((n)=>(
             <li key={n} className='page-item'>
-                <a onClick={()=>paginate(n)} href="!#" className='px-3 py-1 bg-gray-200 transition ease-in-out rounded-md hover:bg-[#EE7214] hover:text-white duration-300'>{n}</a>
+                <a onClick={()=>paginate(n)} className='px-3 py-1 bg-gray-200 transition ease-in-out rounded-md hover:bg-[#EE7214] hover:text-white duration-300 hover:cursor-pointer'>{n}</a>
             </li>
          ))}   
         </ul>
